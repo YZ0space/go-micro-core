f097e46adf0534f872991a0f08ade252ef861920
aka-yz <yue@space.id>
2022-12-13 19:15:58 +0800

    modify log_init.go

--- log_init.go
+++ log_init.go
@@ -2,7 +2,7 @@ package go_micro_core
 
 import (
 	"fmt"
-	log2 "github.com/aka-yz/go-micro-core/providers/config/log"
+	"github.com/aka-yz/go-micro-core/providers/config/log"
 	"github.com/aka-yz/go-micro-core/providers/constants"
 	"go.uber.org/config"
 )
@@ -13,10 +13,10 @@ func initLog(conf config.Provider) {
 		return
 	}
 
-	var cfg log2.Option
+	var cfg log.Option
 	if err := cv.Populate(&cfg); err != nil {
 		panic(err)
 	}
 	fmt.Printf("cfg:%v monitor\n", cfg)
-	log2.InitLogger(&cfg)
+	log.InitLogger(&cfg)
 }
